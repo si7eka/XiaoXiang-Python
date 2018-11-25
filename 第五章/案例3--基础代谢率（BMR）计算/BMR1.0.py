710622e5d036eaf51b6d66b04453915d8487c6e5
si7eka <lizhenggood@126.com>
2018-11-25 17:35:27 +0800

    优化输出和输入

--- 第五章/案例3--基础代谢率（BMR）计算/BMR1.0.py
+++ 第五章/案例3--基础代谢率（BMR）计算/BMR1.0.py
@@ -1,64 +1,43 @@
 """
     作者：李征
-    功能：利用递归绘制分形树
-    版本：1.0
+    功能：计算用户BMR
+    版本：3.0
 
-    turtle库说明：
-    形状绘制函数
-        turtle.forward(distance)
-        画笔向前移动distance距离
-        turtle.backward(distance)
-        笔画向后移动distance距离
-        turtle.right(degree)
-        绘制方向向右旋转degree度
-        turtle.exitonclick()
-        点击关闭图形窗口
 
-                     （左）
-                       |
-              （后）--中心点--（前）
-                       |
-                     （右）
-        更多查询 https://docs.python.org/3/library/turtle.html#module-turtle
 """
-
-import turtle
-
-
-def huizhi_fenxingshu_shuzhi(shuzhichangdu, shuzhisize):
-    """
-    绘制分形树
-    """
-
-    if shuzhichangdu > 5:
-
-
-        # 绘制右侧树枝
-        turtle.pensize(shuzhisize)
-        turtle.forward(shuzhichangdu)
-        turtle.right(20)
-        huizhi_fenxingshu_shuzhi(shuzhichangdu - 15, shuzhisize - 1)
-        if shuzhisize < 3:
-            turtle.color("green")
-        # 绘制左侧树枝
-        turtle.left(40)
-        huizhi_fenxingshu_shuzhi(shuzhichangdu - 15, shuzhisize - 1)
-
-        # 返回树杈节点
-        if shuzhisize > 3:
-            turtle.color("brown")
-        turtle.right(20)
-        turtle.backward(shuzhichangdu)
-
 def main():
     """
-        主函数
+    主函数
     """
-    turtle.color("brown")
+    yes_or_no = input('输入任意键继续，退出请输入no。')
 
-    turtle.left(90)
-    huizhi_fenxingshu_shuzhi(shuzhichangdu=90, shuzhisize=7)
-    turtle.exitonclick()    # 点击关闭图形
+    while yes_or_no != "no":
+        print('请输入以下信息，用空格分割')  # 输入提示
+        input_str = input('性别 体重（KG） 身高（cm） 年龄：')    # 输入内容赋值给变量input_str
+        str_list = input_str.split(' ')    # 使用input.split 以空格分割输入的内容
+        gender = str_list[0]    # 取前面的分割第0位内容即性别
+        weight = float(str_list[1]) # 取第1位内容即体重，并将字符串类型转浮点类型
+        height = float(str_list[2]) # 取第2位内容即身高，并将字符串类型转浮点类型
+        age = int(str_list[3])  # 取第3位内容即年龄，并将字符串类型转整数型
+
+        if gender == "男": # 如果性别等于 男 则按下面公式计算bmr
+            # 男性
+            bmr = (13.7 * weight) + (5.0 * height) - (6.8 * age) + 66
+        elif gender == "女": #  再或者性别等于 女 则按下面公式计算bmr
+            # 女性
+            bmr = (9.6 * weight) + (1.8 * height) - (4.7 * age) + 655
+        else:   # 否则bmr赋值-1
+            bmr = -1
+
+
+        if bmr != -1: # 如果bmr不等于 -1 则输出下面的内容
+            print('您的性别：{}，体重：{}KG，身高：{}cm，年龄：{}岁'.format(gender,weight,height,age)) # 使用{}占位，使用format方法格式化输出将()内的内容按顺序加到前面的{}中
+            print('您的基础代谢率：{}大卡 '.format(bmr))
+        else:    # 否则输出下面内容
+            print('暂不支持这个类型')
+
+        print() # 输出空行，以分割下一次用户执行
+        yes_or_no = input('任意键继续，退出请输入no。')
 
 
 if __name__ == "__main__":
