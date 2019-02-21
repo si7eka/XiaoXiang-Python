e67b92f14afde05ecc9e0cd2795a2eb9987bc6e6
si7eka <lizhenggood@126.com>
2019-02-21 22:20:23 +0800

    更新6.0 爬虫

--- 案例8--空气质量指数计算/空气质量指数计算6.0/空气质量指数计算6.0.py
+++ 案例8--空气质量指数计算/空气质量指数计算6.0/空气质量指数计算6.0.py
@@ -1,12 +1,12 @@
 """
 作者：si7eklz
 功能：空气质量指数AQI计算
-日期：20190219
-版本：5.0
+日期：20190221
+版本：6.0
 
 案例描述：
     利用爬虫实时获取城市空气质量
-
+    
 概念
     网络爬虫
         自动抓取互联网信息；利用互联网数据进行分析，开发产品。
@@ -28,20 +28,83 @@
             text----http相应内容的字符串形式, 即URL对应的页面内容
         参考:http://cn.python-requests.org/zh_CN/latest/
 
+    高效的解析和处理html， beautifulsoup4
 
+    网页解析
+        结构化解析
+        DOM(Document Object Model), 树形结构
 
+                         DOCUMENT
+                            │
+                          <HTML>
+                       ROOT ELEMENT
+               ┌────────────┴────────────┐
+             <head>                    <body> 
+             ELEMENT                   ELEMENT
+               │                         │
+             <title>                    <div> 
+             ELEMENT                   ELEMENT              
+               │                    ┌────┴──────┐
+           "MY TITLE"              <h1>        <p>
+            ELEMENT              ELEMENT     ELEMENT
+                                                |
+                                               <a>
+                                             ELEMENT
+
+    BeautifulSoup解析网页
+        BeautifulSoup
+            用于解析HTML或xml
+            pip install beautifulsoup4
+            improt bs4
+            步骤
+                1. 创建BeautifulSoup对象
+                2. 查询节点
+                   find， 查到第一个满足的节点
+                   find_all，找到所有满足调节的节点
+            
+            创建对象
+                创建BeautifulSoup对象
+                bs = BeautifulSoup(
+                    url,
+                    html_parser, 指定解析器
+                    enoding      指定编码格式（确保和网页编码一致）
+                )
+            
+            查找节点
+            <a href='a.html' class='a_link'>next page</a>
+            可按节点类型， 属性或内容访问
+            按类型查找节点
+                bs.fing_all('a')
+            按属性查找节点
+                bs.find_all('a',href='a.html')
+                bs.find_all('a',href='a.html',string='next page')
+                bs.find_all('a',class_='a_link')
+                    注意：是class_ 因为python有关键字class避免混淆
+            或者bs.find_all('a',{'class':'a_link'})
 """
 import requests
+from bs4 import BeautifulSoup
 
 
-def get_html_text(url):
+def get_city_aqi(city_pinyin):
     """
-        获取url文本
+        获取城市AQI
     """
+    url = 'http://pm25.in/' + city_pinyin
     r = requests.get(url, timeout=30)
-    # print(r.status_code)
-    return r.text
- 
+    soup = BeautifulSoup(r.text, 'lxml')
+    div_list = soup.find_all('div', {'class': 'span1'})
+
+    div_list = soup.find_all('div', {'class': 'span1'})
+
+    city_aqi = []
+    for i in range(8):
+        div_content = div_list[i]
+        caption = div_content.find('div', {'class': 'caption'}).text.strip()
+        value = div_content.find('div', {'class': 'value'}).text.strip()
+
+        city_aqi.append((caption, value))
+    return city_aqi
     
 
 
@@ -50,18 +113,8 @@ def main():
         主函数
     """
     city_pinyin = input('请输入城市全拼：')
-    url = 'http://pm25.in/' + city_pinyin
-    url_text = get_html_text(url)
-
-    aqi_div = '''<div class="span12 data">
-        <div class="span1">
-          <div class="value">
-            '''
-    index = url_text.find(aqi_div)
-    begin_index = index + len(aqi_div)
-    end_index = begin_index + 3
-    aqi_div = url_text[begin_index: end_index]
-    print('空气质量为：{}'.format(aqi_div))
+    city_aqi = get_city_aqi(city_pinyin)
+    print(city_aqi)
 
 if __name__ == '__main__':
     main()
